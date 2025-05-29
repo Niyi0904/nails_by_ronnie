e34119e3a7ad7ba1fa325bd77db0cb44f40f38a3
Niyi0904 <owoyeminiyi2@gmail.com>
2025-05-29 23:00:50 +0100

    changed cors config to allow all origins

--- server/index.js
+++ server/index.js
@@ -16,7 +16,9 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(cookieParser());
-
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
 app.use('/api/auth', authRoutes);
 app.use('/api/booking', bookingRoutes);
 app.use('/api/users', usersRoutes);
