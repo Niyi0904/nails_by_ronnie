dcc74e0a45fe46721ec0b11073fc1834ba8f59b9
Niyi0904 <owoyeminiyi2@gmail.com>
2025-06-04 09:04:01 +0100

    Cart item bug fix

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -113,6 +113,7 @@ const login = async (req, res) => {
       role: user.role,
     }
 
+    console.log('TOKENDATA:', token);
     const token = jwt.sign(tokenData, process.env.JWT_SECRET, { expiresIn: '1m' });
     res.cookie('token', token, {
       httpOnly: true,
