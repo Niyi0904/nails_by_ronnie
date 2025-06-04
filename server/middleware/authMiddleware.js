94284522fc154c0b2f3b0b0409259e817625b4fc
Niyi0904 <owoyeminiyi2@gmail.com>
2025-06-04 08:36:15 +0100

    Cart item bug fix

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -4,12 +4,12 @@ const jwt = require('jsonwebtoken');
 function authenticateUser(req, res, next) {
   console.log('reached');
   console.log('cookies', req.cookies);
-  console.log('Decoded JWT:', user); // << should include `id`
   const token = req.cookies.token;
   if (!token) return res.status(401).json({ message: 'Not authenticated' });
-
+  
   try {
     const user = jwt.verify(token, process.env.JWT_SECRET);
+    console.log('Decoded JWT:', user); // << should include `id`
     req.user = user;
     next();
   } catch (err) {
