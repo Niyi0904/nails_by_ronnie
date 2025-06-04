8420cdaf7065843766f053bc280a173ac44a3341
Niyi0904 <owoyeminiyi2@gmail.com>
2025-06-04 03:14:15 +0100

    fetch bookings by email not id (3)

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -118,7 +118,7 @@ const login = async (req, res) => {
     res.cookie('token', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      sameSite: 'lax'
     });
 
     return res.status(201).json({
